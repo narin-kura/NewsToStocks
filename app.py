3b9c34ec14f0968418cb2248ab585a97de2b817f
narin-kura <k.n.narin@gmail.com>
2026-06-06 13:45:10 -0500

    chore: use K.N.Narin handle in copyright, contact via GitHub

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2025-2026 Kura Nagender Narin (github.com/narin-kura). All rights reserved.
-# Non-commercial use only. Commercial use requires written permission: k.n.narin@gmail.com
+# Copyright (c) 2025-2026 K.N.Narin (github.com/narin-kura). All rights reserved.
+# Non-commercial use only. Commercial use requires written permission: github.com/narin-kura
 # NOT financial advice. See LICENSE for full terms and important disclaimers.
 import os
 import re
